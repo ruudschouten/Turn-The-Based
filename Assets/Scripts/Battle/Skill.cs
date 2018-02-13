f3df8c58e45838abe547a098ae44bca7b0b50533
Ruud Schouten <zzding@live.nl>
2018-02-13 21:06:19 +0100

    Added Skill which has an Area of Effect, which can be shown.

---
+++ Assets/Scripts/Battle/Skill.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Battle {
+    public class Skill : Attack {
+        public bool Heals;
+        public bool Diagonal;
+        public bool HasAoE;
+        public string Name;
+        public int ManaCost;
+        public GameObject HighlightPrefab;
+
+        public int AreaOfEffectSize;
+        public float DamageFallOff; //Per Tile away from source
+
+        public void DrawAoE() {
+            if (AreaOfEffectSize == 0) return;
+            var tileWidth = 2.8f;
+            Vector3 sourcePos = new Vector3(0, 0, 0);
+            //Source highlight
+            GameObject parent = new GameObject("Area of Effect");
+
+            var newPos = new Vector3();
+            for (int n = AreaOfEffectSize; n > 0; n--) {
+                for (int x = 0; x < n + 1; x++) {
+                    var y = n - x;
+                    newPos = new Vector3(sourcePos.x + (x * tileWidth), sourcePos.y, sourcePos.z + (y * tileWidth));
+                    Instantiate(HighlightPrefab, newPos, new Quaternion(), parent.transform);
+
+                    newPos = new Vector3(sourcePos.x + (-x * tileWidth), sourcePos.y, sourcePos.z + (-y * tileWidth));
+                    Instantiate(HighlightPrefab, newPos, new Quaternion(), parent.transform);
+                    if (x != 0 && y != 0) {
+                        newPos = new Vector3(sourcePos.x + (-x * tileWidth), sourcePos.y, sourcePos.z + (y * tileWidth));
+                        Instantiate(HighlightPrefab, newPos, new Quaternion(), parent.transform);
+
+                        newPos = new Vector3(sourcePos.x + (x * tileWidth), sourcePos.y, sourcePos.z + (-y * tileWidth));
+                        Instantiate(HighlightPrefab, newPos, new Quaternion(), parent.transform);
+                    }
+                }
+            }
+            Instantiate(HighlightPrefab, sourcePos, new Quaternion(), parent.transform);
+        }
+    }
+}
---
+++ Assets/Scripts/Inspector/SkillEditor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Scripts.Battle;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts.Inspector {
+    [CustomEditor(typeof(Skill))]
+    public class SkillEditor : Editor {
+        public bool showAreaOfEffect;
+        public override void OnInspectorGUI() {
+            Skill skill = (Skill) target;
+            skill.Name = EditorGUILayout.TextField("Name", skill.Name);
+            skill.Element = (Element)EditorGUILayout.EnumPopup("Element", skill.Element);
+            skill.BoostedBy = (BoostedBy) EditorGUILayout.EnumPopup("Boosted By", skill.BoostedBy);
+            skill.ManaCost = EditorGUILayout.IntField("Mana Cost", skill.ManaCost);
+            skill.Range = EditorGUILayout.IntField("Range", skill.Range);
+            skill.Height = EditorGUILayout.FloatField("Height", skill.Height);
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Damage Modifier");
+            skill.DamageModifier = EditorGUILayout.Slider(skill.DamageModifier, 0.25f, 2.5f);
+            EditorGUILayout.EndHorizontal();
+            skill.Diagonal = EditorGUILayout.Toggle("Diagonal", skill.Diagonal);
+            skill.Heals = EditorGUILayout.Toggle("Heals", skill.Heals);
+            skill.HasAoE = EditorGUILayout.Toggle("Has Area of Effect", skill.HasAoE);
+            if (skill.HasAoE) {
+                skill.HighlightPrefab = (GameObject) EditorGUILayout.ObjectField(skill.HighlightPrefab, typeof(GameObject), true);
+                showAreaOfEffect = EditorGUILayout.Foldout(showAreaOfEffect, "Area of Effect");
+                if (Application.isPlaying) {
+                    if (GUILayout.Button("Show AoE")) {
+                        skill.DrawAoE();
+                    }
+                }
+            }
+            if (showAreaOfEffect) {
+                EditorGUI.indentLevel++;
+                skill.AreaOfEffectSize = EditorGUILayout.IntField("Size", skill.AreaOfEffectSize);
+                skill.DamageFallOff = EditorGUILayout.FloatField("Damage Fall Off", skill.DamageFallOff);
+                EditorGUI.indentLevel--;
+            }
+        }
+    }
+}
