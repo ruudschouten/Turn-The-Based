a6c30460b4f2ca3318e9661cff15a5b886f09b3f
Ruud Schouten <ruud.schouten@outlook.com>
2018-03-08 14:13:10 +0100

    Workshop Resources UI

---
+++ Assets/Scripts/Resource/Resource.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Resource {
+    public class Resource : MonoBehaviour {
+        public string Name;
+        public int Amount;
+
+        public UnityEvent OnValueChanged;
+
+        public void ChangeAmount(int amount) {
+            Amount += amount;
+            OnValueChanged.Invoke();
+        }
+    }
+}
