f231c03a0fdfd63cb2c40f4bba163a809b1042c3
Ruud Schouten <ruud.schouten@outlook.com>
2018-03-09 14:35:01 +0100

    Updated with more visibility

--- Assets/Scripts/UI/ResourceUIManager.cs
+++ Assets/Scripts/UI/ResourceUIManager.cs
@@ -5,8 +5,6 @@ using UnityEngine.UI;
 
 namespace Resource {
     public class ResourceUIManager : MonoBehaviour {
-        public Text PlayerName;
-
         public GameObject PanelParent;
         public GameObject ResourcePanel;
 
@@ -14,29 +12,30 @@ namespace Resource {
 
         private const int PanelHeightPerResource = 35;
         private int _panelTotalHeight = 150;
-        private int index;
+        private int _index;
 
         public void Awake() {
             foreach (var resource in Resources) {
                 UnityAction action = () => UpdateUI(resource);
                 resource.OnValueChanged.AddListener(action);
                 CreateResourcePanel(resource);
-                index++;
+                _index++;
             }
         }
 
         private void CreateResourcePanel(Resource resource) {
             var newPanel = Instantiate(ResourcePanel, PanelParent.transform);
             newPanel.name = string.Format("{0} UI Element", resource.Name);
-            newPanel.transform.Find("ResourceName").GetComponent<Text>().text = resource.Name;
-            newPanel.transform.Find("ResourceValue").GetComponent<Text>().text = resource.Amount.ToString();
-            newPanel.transform.Find("ButtonAdd").GetComponent<Button>().onClick
-                .AddListener(() => resource.ChangeAmount(1));
-            newPanel.transform.Find("ButtonRemove").GetComponent<Button>().onClick
-                .AddListener(() => resource.ChangeAmount(-1));
-            var newY = index * PanelHeightPerResource;
+            newPanel.transform.GetChild(0).GetComponent<Text>().text = resource.Name;
+            newPanel.transform.GetChild(1).GetComponent<Text>().text = resource.Amount.ToString();
+            newPanel.transform.GetChild(2).GetComponent<Button>().onClick
+                .AddListener(() => resource.ChangeAmount(5));
+            newPanel.transform.GetChild(3).GetComponent<Button>().onClick
+                .AddListener(() => resource.ChangeAmount(-5));
+            var newY = _index * PanelHeightPerResource;
             newPanel.transform.position = new Vector3(0, 600 - newY, 0);
         }
+
 
         private void UpdateUI(Resource resource) {
             var panel = PanelParent.transform.Find(string.Format("{0} UI Element", resource.Name));
