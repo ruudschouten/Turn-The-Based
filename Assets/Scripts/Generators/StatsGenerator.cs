1ad22ce13f4a8d2b4ab67a904937f60b0081c840
ruudschouten <ruud.schouten@outlook.com>
2019-10-27 12:55:52 +0100

    Added curly braces to all if statements

--- Assets/Scripts/Generators/StatsGenerator.cs
+++ Assets/Scripts/Generators/StatsGenerator.cs
@@ -84,44 +84,154 @@ public class StatsGenerator : MonoBehaviour
         {
             #region Movement
 
-            if (trait.RadialOnly) character.MoveType = MovementType.Radial;
-            if (trait.DiagonalOnly) character.MoveType = MovementType.Diagonal;
+            if (trait.RadialOnly)
+            {
+                character.MoveType = MovementType.Radial;
+            }
+
+            if (trait.DiagonalOnly)
+            {
+                character.MoveType = MovementType.Diagonal;
+            }
 
             #endregion
 
             #region Additions
 
-            if (trait.MovementAddition.Move != 0) stats.Movement.Move += trait.MovementAddition.Move;
-            if (trait.MovementAddition.Jump != 0) stats.Movement.Jump += trait.MovementAddition.Jump;
-            if (trait.ResourceAddition.Health != 0) stats.Resources.Health += trait.ResourceAddition.Health;
-            if (trait.ResourceAddition.Magic != 0) stats.Resources.Magic += trait.ResourceAddition.Magic;
-            if (Math.Abs(trait.AttributeAddition.Strength) > 0) stats.Attributes.Strength += trait.AttributeAddition.Strength;
-            if (Math.Abs(trait.AttributeAddition.Defense) > 0)  stats.Attributes.Defense += trait.AttributeAddition.Defense;
-            if (Math.Abs(trait.AttributeAddition.Intelligence) > 0)  stats.Attributes.Intelligence += trait.AttributeAddition.Intelligence;
-            if (Math.Abs(trait.AttributeAddition.Resistance) > 0)  stats.Attributes.Resistance += trait.AttributeAddition.Resistance;
-            if (Math.Abs(trait.AttributeAddition.Precision) > 0)  stats.Attributes.Precision += trait.AttributeAddition.Precision;
-            if (Math.Abs(trait.AttributeAddition.Agility) > 0)  stats.Attributes.Agility += trait.AttributeAddition.Agility;
-            if (Math.Abs(trait.AttunementAddition.Fire) > 0) stats.Attunement.Fire += trait.AttunementAddition.Fire;
-            if (Math.Abs(trait.AttunementAddition.Ice) > 0) stats.Attunement.Ice += trait.AttunementAddition.Ice;
-            if (Math.Abs(trait.AttunementAddition.Wind) > 0) stats.Attunement.Wind += trait.AttunementAddition.Wind;
+            if (trait.MovementAddition.Move != 0)
+            {
+                stats.Movement.Move += trait.MovementAddition.Move;
+            }
+
+            if (trait.MovementAddition.Jump != 0)
+            {
+                stats.Movement.Jump += trait.MovementAddition.Jump;
+            }
+
+            if (trait.ResourceAddition.Health != 0)
+            {
+                stats.Resources.Health += trait.ResourceAddition.Health;
+            }
+
+            if (trait.ResourceAddition.Magic != 0)
+            {
+                stats.Resources.Magic += trait.ResourceAddition.Magic;
+            }
+
+            if (Math.Abs(trait.AttributeAddition.Strength) > 0)
+            {
+                stats.Attributes.Strength += trait.AttributeAddition.Strength;
+            }
+
+            if (Math.Abs(trait.AttributeAddition.Defense) > 0)
+            {
+                stats.Attributes.Defense += trait.AttributeAddition.Defense;
+            }
+
+            if (Math.Abs(trait.AttributeAddition.Intelligence) > 0)
+            {
+                stats.Attributes.Intelligence += trait.AttributeAddition.Intelligence;
+            }
+
+            if (Math.Abs(trait.AttributeAddition.Resistance) > 0)
+            {
+                stats.Attributes.Resistance += trait.AttributeAddition.Resistance;
+            }
+
+            if (Math.Abs(trait.AttributeAddition.Precision) > 0)
+            {
+                stats.Attributes.Precision += trait.AttributeAddition.Precision;
+            }
+
+            if (Math.Abs(trait.AttributeAddition.Agility) > 0)
+            {
+                stats.Attributes.Agility += trait.AttributeAddition.Agility;
+            }
+
+            if (Math.Abs(trait.AttunementAddition.Fire) > 0)
+            {
+                stats.Attunement.Fire += trait.AttunementAddition.Fire;
+            }
+
+            if (Math.Abs(trait.AttunementAddition.Ice) > 0)
+            {
+                stats.Attunement.Ice += trait.AttunementAddition.Ice;
+            }
+
+            if (Math.Abs(trait.AttunementAddition.Wind) > 0)
+            {
+                stats.Attunement.Wind += trait.AttunementAddition.Wind;
+            }
 
             #endregion
 
             #region Multipliers
 
-            if (Math.Abs(trait.ResourceModifier.Health) > 0) stats.Resources.Health = (trait.ResourceModifier.Health * stats.Resources.Health); 
-            if (Math.Abs(trait.ResourceModifier.Magic) > 0) stats.Resources.Magic = (trait.ResourceModifier.Magic * stats.Resources.Magic); 
-            if (Math.Abs(trait.MovementModifier.Move) > 0) stats.Movement.Move = (trait.MovementModifier.Move * stats.Movement.Move);
-            if (Math.Abs(trait.MovementModifier.Jump) > 0) stats.Movement.Jump = (trait.MovementModifier.Jump * stats.Movement.Jump);
-            if (Math.Abs(trait.AttributeModifier.Strength) > 0)     stats.Attributes.Strength =     (int) (trait.AttributeModifier.Strength * stats.Attributes.Strength);
-            if (Math.Abs(trait.AttributeModifier.Defense) > 0)      stats.Attributes.Defense =      (int) (trait.AttributeModifier.Defense * stats.Attributes.Defense); 
-            if (Math.Abs(trait.AttributeModifier.Intelligence) > 0) stats.Attributes.Intelligence = (int) (trait.AttributeModifier.Intelligence * stats.Attributes.Intelligence);
-            if (Math.Abs(trait.AttributeModifier.Resistance) > 0)   stats.Attributes.Resistance =   (int) (trait.AttributeModifier.Resistance * stats.Attributes.Resistance); 
-            if (Math.Abs(trait.AttributeModifier.Precision) > 0)    stats.Attributes.Precision =    (int) (trait.AttributeModifier.Precision * stats.Attributes.Precision);
-            if (Math.Abs(trait.AttributeModifier.Agility) > 0)      stats.Attributes.Agility =      (int) (trait.AttributeModifier.Agility * stats.Attributes.Agility);
-            if (Math.Abs(trait.AttunementModifier.Fire) > 0) stats.Attunement.Fire = (int) (trait.AttunementModifier.Fire * stats.Attunement.Fire);
-            if (Math.Abs(trait.AttunementModifier.Ice) > 0)  stats.Attunement.Ice = (int)  (trait.AttunementModifier.Ice *  stats.Attunement.Ice);
-            if (Math.Abs(trait.AttunementModifier.Wind) > 0) stats.Attunement.Wind = (int) (trait.AttunementModifier.Wind * stats.Attunement.Wind); 
+            if (Math.Abs(trait.ResourceModifier.Health) > 0)
+            {
+                stats.Resources.Health = (trait.ResourceModifier.Health * stats.Resources.Health);
+            }
+
+            if (Math.Abs(trait.ResourceModifier.Magic) > 0)
+            {
+                stats.Resources.Magic = (trait.ResourceModifier.Magic * stats.Resources.Magic);
+            }
+
+            if (Math.Abs(trait.MovementModifier.Move) > 0)
+            {
+                stats.Movement.Move = (trait.MovementModifier.Move * stats.Movement.Move);
+            }
+
+            if (Math.Abs(trait.MovementModifier.Jump) > 0)
+            {
+                stats.Movement.Jump = (trait.MovementModifier.Jump * stats.Movement.Jump);
+            }
+
+            if (Math.Abs(trait.AttributeModifier.Strength) > 0)
+            {
+                stats.Attributes.Strength = (int) (trait.AttributeModifier.Strength * stats.Attributes.Strength);
+            }
+
+            if (Math.Abs(trait.AttributeModifier.Defense) > 0)
+            {
+                stats.Attributes.Defense = (int) (trait.AttributeModifier.Defense * stats.Attributes.Defense);
+            }
+
+            if (Math.Abs(trait.AttributeModifier.Intelligence) > 0)
+            {
+                stats.Attributes.Intelligence =
+                    (int) (trait.AttributeModifier.Intelligence * stats.Attributes.Intelligence);
+            }
+
+            if (Math.Abs(trait.AttributeModifier.Resistance) > 0)
+            {
+                stats.Attributes.Resistance = (int) (trait.AttributeModifier.Resistance * stats.Attributes.Resistance);
+            }
+
+            if (Math.Abs(trait.AttributeModifier.Precision) > 0)
+            {
+                stats.Attributes.Precision = (int) (trait.AttributeModifier.Precision * stats.Attributes.Precision);
+            }
+
+            if (Math.Abs(trait.AttributeModifier.Agility) > 0)
+            {
+                stats.Attributes.Agility = (int) (trait.AttributeModifier.Agility * stats.Attributes.Agility);
+            }
+
+            if (Math.Abs(trait.AttunementModifier.Fire) > 0)
+            {
+                stats.Attunement.Fire = (int) (trait.AttunementModifier.Fire * stats.Attunement.Fire);
+            }
+
+            if (Math.Abs(trait.AttunementModifier.Ice) > 0)
+            {
+                stats.Attunement.Ice = (int) (trait.AttunementModifier.Ice * stats.Attunement.Ice);
+            }
+
+            if (Math.Abs(trait.AttunementModifier.Wind) > 0)
+            {
+                stats.Attunement.Wind = (int) (trait.AttunementModifier.Wind * stats.Attunement.Wind);
+            }
 
             #endregion
 
