536a8933a3aa5bb21bff8039c336243c22ff81e2
ruudschouten <ruud.schouten@outlook.com>
2019-11-02 18:36:27 +0100

    Simplified by setting Button listener in Editor instead via code

--- Assets/Scripts/UI/GameOverUIManager.cs
+++ Assets/Scripts/UI/GameOverUIManager.cs
@@ -3,41 +3,42 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class GameOverUIManager : MonoBehaviour {
-    public GameObject GameOverPanel;
+namespace UI
+{
+    public class GameOverUIManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject gameOverPanel;
+        [SerializeField] private TurnManager turnManager;
+        [SerializeField] private Text winLoseText;
 
-    public TurnManager TurnManager;
+        private void Start()
+        {
+            Hide();
+        }
 
-    private Text _winLoseText;
-    private Button _btnRestart;
+        public void Show()
+        {
+            winLoseText.text = GetWinLose();
+            gameOverPanel.SetActive(true);
+        }
 
-    // Use this for initialization
-    void Start() {
-        Hide();
-        _winLoseText = GameOverPanel.transform.GetChild(1).GetComponent<Text>();
-        _btnRestart = GameOverPanel.transform.GetChild(2).GetComponent<Button>();
-        _btnRestart.onClick.AddListener(Restart);
-    }
+        public void Hide()
+        {
+            gameOverPanel.SetActive(false);
+        }
 
-    public void Show() {
-        _winLoseText.text = GetWinLose();
-        GameOverPanel.SetActive(true);
-    }
+        public void Restart()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
 
-    public void Hide() {
-        GameOverPanel.SetActive(false);
-    }
-
-    private void Restart() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-
-    private string GetWinLose() {
-        var text = "";
-        var winner = TurnManager.Winner;
-        var loser = TurnManager.Loser;
-        text = string.Format("Winner: [{0}]{1}\nLoser: [{2}]{3}", winner.Color, winner.Name,
-            loser.Color, loser.Name);
-        return text;
+        private string GetWinLose()
+        {
+            var text = "";
+            var winner = turnManager.Winner;
+            var loser = turnManager.Loser;
+            text = $"Winner: [{winner.Color}]{winner.Name}\nLoser: [{loser.Color}]{loser.Name}";
+            return text;
+        }
     }
 }
